b81f423954310e25cab823cf93417d7520c9ed2d
mtinner <m1tinner@gmail.com>
2015-05-16 17:18:44 +0200

    init

---
+++ wifi-sta-tcp-client/include/user_config.h
@@ -0,0 +1,14 @@
+#ifndef __USER_CONFIG_H__
+#define __USER_CONFIG_H__
+
+#define USE_WIFI_MODE		STATIONAP_MODE
+#define WIFI_CLIENTSSID		"de_kaatz"
+#define WIFI_CLIENTPASSWORD	"Access=true"
+#define WIFI_AP_NAME		""
+#define WIFI_AP_PASSWORD	""
+#define TCPSERVERIP			"10.0.0.13"
+#define TCPSERVERPORT		8610
+#define PLATFORM_DEBUG		true
+//#define LWIP_DEBUG		true
+
+#endif
